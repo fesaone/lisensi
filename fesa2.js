60f4a7b1bf49b80a0211a92cdd373859a3b60db7
FESA ONE <133826879+fesaone@users.noreply.github.com>
2023-05-30 00:38:04 +0700

    Create fesa2.js

---
+++ fesa2.js
@@ -0,0 +1,39 @@
+fetch('https://cdn.jsdelivr.net/gh/fesaone/lisensi@main/data2.json')
+  .then(response => response.json())
+  .then(data => {
+    if (Array.isArray(data)) {
+      const currentURL = new URL(window.location.href);
+      const currentDomain = currentURL.hostname.startsWith("www.") ? currentURL.hostname.substring(4) : currentURL.hostname;
+
+      console.log('Current Domain:', currentDomain);
+
+      const registeredDomain = data.find(item => {
+        const itemURL = new URL(item.url);
+        const itemDomain = itemURL.hostname.startsWith("www.") ? itemURL.hostname.substring(4) : itemURL.hostname;
+        return itemDomain === currentDomain;
+      });
+
+      if (registeredDomain) {
+        const currentDate = new Date().toISOString().split('T')[0];
+        const expirationDate = registeredDomain.expirationDate;
+
+        console.log('Current Date:', currentDate);
+        console.log('Expiration Date:', expirationDate);
+
+        if (currentDate > expirationDate) {
+          console.log('Redirecting to Google...');
+          window.location.href = 'https://www.jsdelivr.com/';
+        } else {
+          console.log('Domain is registered.');
+        }
+      } else {
+        console.log('Domain is not registered.');
+        window.location.href = 'https://gomotive.com/';
+      }
+    } else {
+      console.error('Invalid data format. Expected an array.');
+    }
+  })
+  .catch(error => {
+    console.error('Terjadi kesalahan dalam mengambil data JSON:', error);
+  });
